39e45e4cd392dc72c73b8fecf412699eb900a811
neiterman21 <neiterman21@gmail.com>
2023-04-23 16:22:20 +0300

    fixed missing ()

--- Demo.cpp
+++ Demo.cpp
@@ -29,7 +29,7 @@ int main() {
     cout << c++ << endl;
     cout << --c << endl;
 
-    cout << "c >=b ? : " << c >= b << endl;
+    cout << "c >=b ? : " << (c >= b) << endl;
     if (a > 1.1) cout << " a is bigger than 1.1" << endl;
     else cout << " a is smaller than 1.1" << endl;
 
